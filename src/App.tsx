89de40e69100162eb6f8eb7948baa48bf18a080c
merturl <whdgus963@naver.com>
2020-03-15 22:12:53 +0900

    Add startnumber

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,27 @@ import storage from './lib/storage';
 const App = () => {
   const [OFFSET, setOFFSET] = useState(0);
   const [isEnd, setIsEnd] = useState(false);
+  const [startNumber, setStartNumber] = useState('0');
   const [URL, setURL] = useState('');
 
   useEffect(() => {
     const storagedURL = storage.get('URL');
+    const storagedStartNumber = storage.get('StartNumber');
     const handleScrolling = () => {
       const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-  
+
       if (scrollTop + clientHeight === scrollHeight) {
         setIsEnd(true);
       } else {
         setIsEnd(false);
       }
     };
+
     if (storagedURL) {
       setURL(storagedURL);
+    }
+    if (storagedStartNumber) {
+      setStartNumber(storagedStartNumber);
     }
 
     document.addEventListener('scroll', handleScrolling);
@@ -34,25 +40,41 @@ const App = () => {
     if (isEnd) setOFFSET(newOFFSET);
   }, [isEnd]);
 
-  const makeImage = (URL: string) => {
-    return Array(10+OFFSET).fill(0).map((_, i) => <Picture key={i} URL={URL} id={i} />);
+  const makeImage = (URL: string, startNumber: string) => {
+    return Array(10 + OFFSET).fill(0).map((_, i) => <Picture key={i} URL={URL} id={i + parseInt(startNumber)} />);
   }
 
-  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleURLSave = (e: React.MouseEvent<HTMLButtonElement>) => {
     storage.set('URL', URL);
   }
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleStartNumberSave = (e: React.MouseEvent<HTMLButtonElement>) => {
+    storage.set('StartNumber', startNumber);
+  }
+
+  const handleURLChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newURL = e.target.value;
 
     setURL(newURL);
   }
 
+  const handleStartNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newStartNumber = e.target.value;
+
+    setStartNumber(newStartNumber);
+  }
+
   return (
     <>
-      <PathInput URL={URL} onChange={handleOnChange} onClick={handleOnClick} />
+      <PathInput URL={URL}
+        startNumber={startNumber}
+        onURLChange={handleURLChange}
+        onStartNumberChange={handleStartNumberChange}
+        onURLSave={handleURLSave}
+        onStartNumberSave={handleStartNumberSave} 
+      />
       <div className={'grid-container'}>
-        {makeImage(URL)}
+        {makeImage(URL, startNumber)}
       </div>
     </>
   )
--- src/components/PathInput.tsx
+++ src/components/PathInput.tsx
@@ -3,25 +3,37 @@ import styled from 'styled-components';
 
 interface PathInputProps {
   URL: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  startNumber: string;
+  onURLChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onStartNumberChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onURLSave: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onStartNumberSave: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const PathInputBlock = styled.div`
-
+  display: flex;
+  flex-direction: column;
   button {
 
-  }  
+  }
   input {
   }
 `
 
 
-const PathInput = ({URL, onChange, onClick}: PathInputProps) => {
+const PathInput = ({ URL, startNumber, onURLChange, onStartNumberChange, onURLSave, onStartNumberSave }: PathInputProps) => {
+  console.log(startNumber);
   return (
     <PathInputBlock>
-      <input onChange={onChange} value={URL} placeholder={`http://localhost:9999/image/result`}></input>
-      <button onClick={onClick}>Save URL</button>
+      <div>
+        <input type="text" onChange={onURLChange} value={URL} placeholder={`http://localhost:9999/image/result`}></input>
+        <button onClick={onURLSave}>Save URL</button>
+      </div>
+      <div>
+        <input type="text" onChange={onStartNumberChange} value={startNumber} placeholder={'123'}></input>
+        <button onClick={onStartNumberSave}>Save Start Number</button>
+      </div>
     </PathInputBlock>
   )
 }
